667a7c5882c3f726b05be02617274c1f40632ac3
Prieto Ezequiel <72413070+zequi-pv@users.noreply.github.com>
2024-09-02 12:30:24 -0300

    Fixed raylib

--- TP2Algebra/main.cpp
+++ TP2Algebra/main.cpp
@@ -1,6 +1,52 @@
+#include <iostream>
 #include "raylib.h"
 
-void main()
+void Update();
+void Draw();
+
+int main(void)
+{
+	srand(time(NULL));
+
+	const int screenWidth = 800;
+	const int screenHeight = 500;
+	int maxDegrees = 360;
+	//const int maxPoints = 3;
+
+	vector3 startPos = { 0.0f, 0.0f, 0.0f, };
+
+	Vector3 vectorA = { rand(0, maxDegrees), rand(0, maxDegrees, rand(0, maxDegrees) };
+	Vector3 vectorB = { 0.0f, 0.0f, 0.0f };
+	Vector3 vectorC = { 0.0f, 0.0f, 0.0f };
+
+	//Vector3 points[maxPoints] = { a, b, c };
+
+	InitWindow(screenWidth, screenHeight, "TP2 Algebra");
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+		Update();
+		Draw();
+	}
+
+	CloseWindow();
+
+	return 0;
+}
+
+void Update()
 {
 
+}
+
+void Draw()
+{
+	BeginDrawing();
+
+	ClearBackground(RAYWHITE);
+
+
+	EndDrawing();
 }
